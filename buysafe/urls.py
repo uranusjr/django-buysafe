51ea6f13c25a3c953a1c92e6ed3a122b909d6fcb
Tzu-ping Chung <tpchung@lbstek.com>
2013-05-03 17:26:08 +0800

    Remove obsolute redirection

--- buysafe/urls.py
+++ buysafe/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import patterns
-from django.views.generic import RedirectView
-from django.core.urlresolvers import reverse_lazy
 
 
 urlpatterns = patterns(
     'buysafe.views',
-    (r'^$', RedirectView.as_view(url=reverse_lazy('buysafe.views.entry'))),
     (r'^entry/(?P<order_id>\d+)/$', 'entry'),
     (r'^start/$', 'start'),
     (r'^success/(?P<payment_type>[01])/$', 'success'),
