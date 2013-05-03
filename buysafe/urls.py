ca917fa28c5bf8fe3c431868951f429c48b58e0a
Tzu-ping Chung <tpchung@lbstek.com>
2013-05-03 18:17:54 +0800

    Add view label to buysafe_start

--- buysafe/urls.py
+++ buysafe/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'buysafe.views',
     url(r'^entry/(?P<order_id>\d+)/$', 'entry', name='buysafe_pay'),
-    (r'^start/$', 'start'),
+    url(r'^start/$', 'start', name="buysafe_start"),
     (r'^success/(?P<payment_type>[01])/$', 'success'),
     (r'^fail/(?P<payment_type>[01])/$', 'fail'),
     (r'^check/(?P<payment_type>[01])/$', 'check')
