b318ee31fb1bbdb959a1bd5c57eb7785580e6516
Tzu-ping Chung <tpchung@lbstek.com>
2013-05-06 11:51:54 +0800

    Auto parse forms

---
+++ buysafe/utils.py
@@ -0,0 +1,64 @@
+import importlib
+from django.conf import settings
+from django.db.models.loading import get_model
+from buysafe.models import PaymentMethod
+from buysafe.forms import BuySafeReceiveForm, WebATMReceiveForm
+
+
+PAYMENT_RECEIVE_FORMS = {
+    PaymentMethod.TYPE_BUYSAFE: BuySafeReceiveForm,
+    PaymentMethod.TYPE_WEBATM: WebATMReceiveForm
+}
+
+
+def default_order_info_handler(**kwargs):
+    order_id = kwargs['order_id']
+    Order = get_model('shop', 'Order')
+    order = Order.objects.get(id=int(order_id))
+    return {
+        'price': order.total,
+        'order_info': unicode(order),
+        'order_id': order.id,
+        'name': order.billing_detail_first_name,
+        'phone': order.billing_detail_phone,
+        'email': order.billing_detail_email
+    }
+
+
+def call_handler(name, default_handler, *args, **kwargs):
+    """
+    Call the handler with `name` set in settings.py with provided arguments,
+    and fallback to `default_handler` if the name is not set. If
+    `default_handler` is none and the name is not set, nothing happends and
+    None will be returned.
+    """
+    try:
+        handler_name = getattr(settings, name)
+    except AttributeError:
+        if default_handler is not None:
+            result = default_handler(*args, **kwargs)
+        else:
+            result = None
+    else:
+        components = handler_name.split('.')
+        module_name, function_name = '.'.join(components[:-1]), components[-1]
+        module = importlib.import_module(module_name)
+        result = getattr(module, function_name)(*args, **kwargs)
+    return result
+
+
+def call_handler_and_return(name, default_response, *args, **kwargs):
+    return call_handler(name, None, *args, **kwargs) or default_response
+
+
+def get_payment_form(payment_type, querydict):
+    methods = PaymentMethod.enabled.filter(payment_type=payment_type)
+    form = None
+    form_type = PAYMENT_RECEIVE_FORMS[payment_type]
+    for method in methods:
+        password = method.password
+        form = form_type(password, request.POST)
+        if form.is_valid():
+            break
+        else:
+            form = None
--- buysafe/views.py
+++ buysafe/views.py
@@ -1,45 +1,23 @@
 # -*- coding: utf8
 
-import importlib
-from django.conf import settings
-from django.db.models.loading import get_model
 from django.http import HttpResponseBadRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.shortcuts import render
 from buysafe.models import PaymentMethod
-from buysafe.forms import (
-    SunTechReceiveForm,
-    BuySafeReceiveForm, BuySafeSendForm,
-    WebATMSendForm, WebATMReceiveForm
+from buysafe.forms import SunTechReceiveForm, BuySafeSendForm, WebATMSendForm
+from buysafe.utils import (
+    call_handler, call_handler_and_return, default_order_info_handler,
+    get_payment_form
 )
 
 
 BUYSAFE_STORE_PASSWORD = 'yourkey2013'
 
-PAYMENT_RECEIVE_FORMS = {
-    PaymentMethod.TYPE_BUYSAFE: BuySafeReceiveForm,
-    PaymentMethod.TYPE_WEBATM: WebATMReceiveForm
-}
-
 PAYMENT_SEND_FORMS = {
     PaymentMethod.TYPE_BUYSAFE: BuySafeSendForm,
     PaymentMethod.TYPE_WEBATM: WebATMSendForm
 }
-
-
-def default_handler(**kwargs):
-    order_id = kwargs['order_id']
-    Order = get_model('shop', 'Order')
-    order = Order.objects.get(id=int(order_id))
-    return {
-        'price': order.total,
-        'order_info': unicode(order),
-        'order_id': order.id,
-        'name': order.billing_detail_first_name,
-        'phone': order.billing_detail_phone,
-        'email': order.billing_detail_email
-    }
 
 
 def entry(request, order_id, template='buysafe/entry.html'):
@@ -52,18 +30,15 @@ def start(request, template='buysafe/start.html'):
     for k in request.POST:
         keyword_args[k] = request.POST[k]
 
-    try:
-        handler_name = settings.BUYSAFE_FORM_VALUES_GENERATOR
-    except AttributeError:
-        info = default_handler(**keyword_args)
-    else:
-        components = handler_name.split('.')
-        module_name, function_name = '.'.join(components[:-1]), components[-1]
-        module = importlib.import_module(module_name)
-        info = getattr(module, function_name)(**keyword_args)
+    info = call_handler(
+        'BUYSAFE_FORM_VALUES_GENERATOR',
+        default_order_info_handler,
+        **keyword_args
+    )
 
     forms = []
-    for method in PaymentMethod.enabled.all():
+    payment_methods = PaymentMethod.enabled.all()
+    for method in payment_methods:
         form_model = PAYMENT_SEND_FORMS[method.payment_type]
         info['store_name'] = method.store_id
         form = form_model(
@@ -76,52 +51,78 @@ def start(request, template='buysafe/start.html'):
     context = {
         'forms': forms
     }
-    return render(request, template, context)
+    return call_handler_and_return(
+        'BUYSAFE_START_HANDLER', render(request, template, context),
+        forms=forms, payment_methods=payment_methods
+    )
 
 
 @csrf_exempt
 @require_POST
 def success(request, payment_type, template='buysafe/success.html'):
     payment_type = int(payment_type)
-    form_type = PAYMENT_RECEIVE_FORMS[payment_type]
-    form = form_type(BUYSAFE_STORE_PASSWORD, request.POST)
-    if not form.is_valid():
-        return HttpResponseBadRequest()
+    form = get_payment_form(payment_type, request.POST)
+    if form is None:
+        return call_handler_and_return(
+            'BUYSAFE_SUCCESS_INVALID_HANDLER', HttpResponseBadRequest(),
+            form=form
+        )
     send_type = form.cleaned_data['send_type']
     if send_type == SunTechReceiveForm.SEND_TYPE.BACKGROUND:
-        return HttpResponse()
+        return call_handler_and_return(
+            'BUYSAFE_SUCCESS_BACKGROUND_HANDLER', HttpResponse(),
+            form=form, cleaned_data=form.cleaned_data
+        )
     context = {
         'data': form.cleaned_data
     }
-    return render(request, template, context)
+    return call_handler_and_return(
+        'BUYSAFE_SUCCESS_RENDER_HANDLER', render(request, template, context),
+        form=form, cleaned_data=form.cleaned_data
+    )
 
 
 @csrf_exempt
 @require_POST
 def fail(request, payment_type, template='buysafe/fail.html'):
     payment_type = int(payment_type)
-    form_type = PAYMENT_RECEIVE_FORMS[payment_type]
-    form = form_type(BUYSAFE_STORE_PASSWORD, request.POST)
-    if not form.is_valid():
-        return HttpResponseBadRequest()
+    form = get_payment_form(payment_type, request.POST)
+    if form is None:
+        return call_handler_and_return(
+            'BUYSAFE_FAIL_INVALID_HANDLER', HttpResponseBadRequest(), form=form
+        )
     send_type = form.cleaned_data['send_type']
     if send_type == SunTechReceiveForm.SEND_TYPE.BACKGROUND:
-        return HttpResponse()
+        return call_handler_and_return(
+            'BUYSAFE_FAIL_BACKGROUND_HANDLER', HttpResponse(),
+            form=form, cleaned_data=form.cleaned_data
+        )
     context = {
         'data': form.cleaned_data
     }
-    return render(request, template, context)
+    return call_handler_and_return(
+        'BUYSAFE_FAIL_RENDER_HANDLER', render(request, template, context),
+        form=form, cleaned_data=form.cleaned_data
+    )
 
 
 @csrf_exempt
 @require_POST
 def check(request, payment_type):
     payment_type = int(payment_type)
-    form_type = PAYMENT_RECEIVE_FORMS[payment_type]
-    form = form_type(BUYSAFE_STORE_PASSWORD, request.POST)
-    if not form.is_valid():
-        return HttpResponseBadRequest()
+    form = get_payment_form(payment_type, request.POST)
+    if form is None:
+        return call_handler_and_return(
+            'BUYSAFE_CHECK_INVALID_HANDLER', HttpResponseBadRequest(),
+            form=form
+        )
     send_type = form.cleaned_data['send_type']
     if send_type == SunTechReceiveForm.SEND_TYPE.BACKGROUND:
+        return call_handler_and_return(
+            'BUYSAFE_CHECK_HANDLER', HttpResponse('0000'),
+            form=form, cleaned_data=form.cleaned_data
+        )
         return HttpResponse('0000')
-    return HttpResponseBadRequest()
+    return call_handler_and_return(
+        'BUYSAFE_CHECK_INVALID_HANDLER', HttpResponseBadRequest(), form=form
+    )
