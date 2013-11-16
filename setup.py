5cf9b267f13d2341d4898e99341519078c4399d9
Tzu-ping Chung <uranusjr@gmail.com>
2013-11-17 00:14:43 +0800

    Fix some missing info

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 import os
 from setuptools import setup
+import buysafe
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+VERSION = buysafe.__version__
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-buysafe',
-    version='0.1',
+    version=VERSION,
     packages=['buysafe'],
     include_package_data=True,
     license='BSD License',
     description='Django wrapper for SunTech BuySafe (TM).',
     long_description=README,
-    url='http://www.example.com/',
-    author='Your Name',
-    author_email='yourname@example.com',
+    url='http://github.com/uranusjr/django-buysafe',
+    author='Tzu-ping Chung',
+    author_email='uranusjr@gmail.com',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
